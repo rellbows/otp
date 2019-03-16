bcf9e3858c8fda0a3127272f7d0816c4939c6053
ellisry <ellisry@oregonstate.edu>
2019-03-16 13:59:44 -0700

    Initial setup of otp_enc_d script

---
+++ otp_enc_d.c
@@ -0,0 +1,24 @@
+/*
+** Filename: otp_end_d.c
+** Description: Daemon script that takes plaintext and a cipher key sent
+** over a socket connection from otp_enc script and encrypts the plaintext
+** Author: Ryan Ellis
+** Class: CS344 - Operating Systems
+** Citations:
+** -Most of the boilerplate connection code is based of the 'server.
+** c' from 'beejs guide to network programming'
+** http://beej.us/guide/bgnet/
+** -Used an existing project of mine for alot of the code as well - ftserver
+**  ftclient
+**  https://github.com/rellbows/ftserv_ftclient
+*/ 
+
+#include "socket_helpers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+
+int main(){
+
+	return 0;
+}
